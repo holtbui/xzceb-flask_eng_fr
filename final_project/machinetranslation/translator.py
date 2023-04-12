6b4faa587e95f2d7e9c4a35972b98a32f562ad31
holtbui <holt.bui@ibm.com>
2023-04-12 17:26:46 -0400

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -2,7 +2,7 @@
 This module uses Watoson Language Translator-n9 to translate text
 """
 import os
-#import json
+import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
